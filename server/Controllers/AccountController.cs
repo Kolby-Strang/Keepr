ceaabc8dd46dabc6ee12a6a777c4088209228057
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-12-08 16:39:07 -0700

    BackEnd Complete!!!

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -5,12 +5,14 @@ namespace Keepr.Controllers;
 public class AccountController : ControllerBase
 {
   private readonly AccountService _accountService;
-  private readonly Auth0Provider _auth0Provider;
+  private readonly VaultsService _vaultsService;
+  private readonly Auth0Provider _a0;
 
-  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  public AccountController(AccountService accountService, Auth0Provider auth0Provider, VaultsService vaultsService)
   {
     _accountService = accountService;
-    _auth0Provider = auth0Provider;
+    _a0 = auth0Provider;
+    _vaultsService = vaultsService;
   }
 
   [HttpGet]
@@ -19,12 +21,29 @@ public class AccountController : ControllerBase
   {
     try
     {
-      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
       return Ok(_accountService.GetOrCreateProfile(userInfo));
     }
     catch (Exception e)
     {
       return BadRequest(e.Message);
+    }
+  }
+
+  [HttpGet("vaults")]
+  [Authorize]
+  public async Task<ActionResult<List<Vault>>> GetMyVaults()
+  {
+    try
+    {
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      string userId = userInfo.Id;
+      List<Vault> vaults = _vaultsService.GetVaultsByProfileId(userId);
+      return Ok(vaults);
+    }
+    catch (Exception err)
+    {
+      return BadRequest(err.Message);
     }
   }
 }
